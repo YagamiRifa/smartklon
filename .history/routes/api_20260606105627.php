94e00e67bdc62d913e446649cebbe1f29fa1b342
YagamiRifa <49101747+YagamiRifa@users.noreply.github.com>
2026-07-18 02:09:59 +0700

    chore: remove .history from git and add to gitignore

--- .history/routes/api_20260606105627.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Api\RfidController;
-use App\Http\Controllers\Api\ScannerController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/rfid/scan', [RfidController::class, 'scan']);
-Route::post('/scanner/batch', [ScannerController::class, 'storeFromScanner']);
